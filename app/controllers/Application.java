0c66c124489f0b37d3245bf00ed46b21a5e8c96e
anchovyimo <anchovy.0imo@gmail.com>
2015-05-14 00:08:12 +0900

    start 1

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,35 +1,14 @@
 package controllers;
 
-import models.User;
-import play.data.Form;
-import play.mvc.Controller;
-import play.mvc.Result;
-import views.html.login;
+import play.*;
+import play.mvc.*;
 
-import static views.html.index.render;
+import views.html.*;
 
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(render());
-    }
-
-    public static Result login() {
-        return ok(login.render(Form.form(User.class)));
-    }
-
-    public static Result authenticate() {
-        Form<User> loginForm = Form.form(User.class).bindFromRequest();
-        if (loginForm.hasErrors()) {
-            return badRequest(login.render(loginForm));
-        }
-        session().clear();
-        session("name", loginForm.get().name);
-        return redirect(routes.Application.index());
+        return ok(index.render("Your new application is ready."));
     }
 
 }
-
-
-
-
